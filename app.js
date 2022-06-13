9afe707502f1cf8b8e758b4f2dae09b7f746ec20
Mohamad Masri <super-2021@hotmail.com>
2022-06-13 21:01:46 +0300

    all in 1

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors())
+const excelToJson = require('convert-excel-to-json');
+
+
+
+
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+
+app.get("/", function (req, res) {
+ res.send("<h1>Hello World!</h1>")
+})
+
+app.get("/menu", function (req, res) {
+  const result = excelToJson({
+    sourceFile: './data.xlsx',
+    sheets: ['menu']
+})
+  res.send(result)
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
