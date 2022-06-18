a0a4e6dc51025f541f48374b4f1f06a6e8988905
Mohamad Masri <super-2021@hotmail.com>
2022-06-18 12:04:28 +0300

    nodemon node mailer

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const excelToJson = require('convert-excel-to-json');
 var cors = require('cors')
 const app=express()
 app.use(cors());
+const path=require('path');
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
-
-
+// app.use(bodyParser.json())
+  
 
 // use the express-static middleware
 app.use(express.static("public"))
@@ -18,7 +18,7 @@ app.use(express.static("public"))
 // define the first route
 
 app.get("/", function (req, res) {
- res.send("<h1>Hello World!</h1>")
+ res.sendFile(__dirname+'/tasty/contact.html')
 })
 
 app.get("/menu", function (req, res) {
